3d20a767ab06b41fe2ba68add79d319004932854
Kavya <kavya@Cherrys-MacBook-Air-3.local>
2026-03-13 17:19:58 +0530

    reconfiguring files

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -5,7 +5,7 @@ terraform {
     key            = "modules-terraform-01/terraform.tfstate"
     region         = "ap-south-1"
 
-    dynamodb_table = "terraform-locking-18"
+    dynamodb_table = "Terraform-locking-18"
 
     encrypt = true
   }
